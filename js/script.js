090729f84f2154957c51c8de93b0d8d822fc9e6e
lucasdcht <lucasdouchet17112003@gmail.com>
2022-11-03 12:29:27 +0100

    feat: carousel & form error handler

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 const hamburger = document.querySelector('.burger-menu')
 const linkList = document.querySelector('.link-container')
+const contactForm = document.querySelector('form')
+const mailInput = document.getElementById('emailInput')
+const submitButton = document.getElementById("submitButton")
+const errorMessage = document.getElementById('errorMessage')
+
+const handleForm = (event) =>  {
+    event.preventDefault();
+}
+
+contactForm.addEventListener('submit', handleForm);
 
 hamburger.addEventListener('click',  () => {
     hamburger.classList.toggle('is-active')
     linkList.classList.toggle('is-open')
+})
+
+submitButton.addEventListener('click', () => {
+    if(mailInput.value.length === 0) {
+        mailInput.style.border = '2px solid red'
+    }
+
+    if(mailInput.value.length !== 0) {
+        mailInput.style.border = 'none'
+    }
 })
